f9d0c0c0f1c3b8dd04c434fcc214bb70d8554f41
ifgeny87 <ifgeny87@gmail.com>
2024-08-09 20:37:17 +0500

    npx nx g @nx/node:application apps/my-node-app --docker --pascalCaseFiles --swcJest

---
+++ apps/my-node-app/src/main.ts
@@ -0,0 +1,6 @@
+import { myLib01 } from 'my-lib01';
+import { myLib02 } from 'my-lib02';
+
+console.log('Hello World');
+console.log(myLib01());
+console.log(myLib02());
---
+++ e2e/my-node-app-e2e/jest.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default {
+	displayName: 'my-node-app-e2e',
+	preset: '../../jest.preset.js',
+	setupFiles: ['<rootDir>/src/test-setup.ts'],
+	testEnvironment: 'node',
+	transform: {
+		'^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+	},
+	moduleFileExtensions: ['ts', 'js', 'html'],
+	coverageDirectory: '../../coverage/e2e/my-node-app-e2e',
+};
